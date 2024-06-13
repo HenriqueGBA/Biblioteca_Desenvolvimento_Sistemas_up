124dd104bb2bb67fbcaf09d0db1258e426d3b3b2
LucasDNX <andre_lucaslencina@hotmail.com>
2024-06-13 17:19:00 -0300

    login incompleto

---
+++ view/loginView.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <input type="text" name="usuario" placeholder="Usuário"><br><br>
+        <input type="password" name="senha" placeholder="Digite sua senha"><br><br>
+        <input type="submit" name="login" value="Login">
+    </form>
+</body>
+</html>
